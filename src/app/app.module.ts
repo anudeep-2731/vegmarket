058165c3b57c3e665516db881264ba28408a3ff6
Anudeep <basvaanu9125@gmail.com>
2022-06-18 13:00:02 +0530

    routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import {Routes, RouterModule} from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+const routes: Routes =[
+{path : 'products/id',component: ProductDetailsComponent},
+{path : 'products',component: ProductListComponent},
+{path : '', redirectTo: '/products' , pathMatch: 'full'},
+{path : '**', redirectTo: '/products' , pathMatch: 'full'}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailsComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
