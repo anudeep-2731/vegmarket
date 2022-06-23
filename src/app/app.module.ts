5dd7c3c9f99093026706e698dd1155712681b445
Anudeep <basvaanu9125@gmail.com>
2022-06-23 10:27:56 +0530

    checkout page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import {Routes, RouterModule} from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
 const routes: Routes =[
+  {path : 'checkout',component: CheckOutComponent},
 {path : 'cart',component: ShoppingCartComponent},
 {path : 'products/id',component: ProductDetailsComponent},
 {path : 'products',component: ProductListComponent},
@@ -20,7 +22,8 @@ const routes: Routes =[
     AppComponent,
     ProductListComponent,
     ProductDetailsComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    CheckOutComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
